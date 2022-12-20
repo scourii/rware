a6de914c3661607c3b2331681555b8491d8e3c4a
scouri <eneblank19@protonmail.com>
2022-12-20 20:17:05 +0000

    updated dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,12 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [tinklj "0.1.6-SNAPSHOT"]]
+                 [tinklj "0.1.6-SNAPSHOT"]
+                 [org.clojure/tools.cli "1.0.214"]
+                 [buddy/buddy-core "1.10.413"]]
   :main ^:skip-aot rware.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+
+
