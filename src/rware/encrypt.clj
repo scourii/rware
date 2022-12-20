58fd8bfb618858214bf8cd967dd94d30d25c9dbc
scouri <eneblank19@protonmail.com>
2022-12-20 20:14:52 +0000

    Update encrypt.clj

--- src/rware/encrypt.clj
+++ src/rware/encrypt.clj
@@ -1,24 +1,27 @@
 (ns rware.encrypt
-  (:require [tinklj.encryption.aead :refer [encrypt]]
-            [tinklj.primitives :as primitives]
+  (:require [tinklj.primitives :as primitives]
             [tinklj.config :refer [register]]
             [tinklj.keys.keyset-handle :as keyset-handles]
+            [tinklj.keysets.keyset-storage :as keyset-storage]
             [tinklj.encryption.aead :as sut]
+            [buddy.core.nonce :as nonce]
             [clojure.java.io :refer [file]]
             [clojure.test :refer [is]]))
 
 (register :aead)
 
-(defn encrypt-test []
-  (let [plain-text (slurp "src/rware/textfile.txt")
+(defn encrypt-test 
+  [path]
+  (let [plain-text (slurp path)
         keyset-handle (keyset-handles/generate-new :aes128-gcm)
         primitive (primitives/aead keyset-handle)
-        aad (.getBytes "Salt")
+        aad (nonce/random-bytes 16)
         encrypted (sut/encrypt primitive
                                (.getBytes plain-text)
-                               aad)
-        decrypted (sut/decrypt primitive
-                               encrypted
                                aad)]
-    (println "Plain-text:" plain-text)
-    (println "Encrypted:"(String. encrypted))))
+    (keyset-storage/write-clear-text-keyset-handle keyset-handle "Key")
+    (spit path (String. encrypted))))
+
+(defn decrypt
+  [path key]
+  )
