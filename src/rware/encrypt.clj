5e64256140aa7508aed889982d24b062820abf25
scouri <eneblank19@protonmail.com>
2023-03-14 04:41:40 +0000

    more secure, decrypt-file fix soontm

--- src/rware/encrypt.clj
+++ src/rware/encrypt.clj
@@ -2,25 +2,10 @@
   (:require [rware.bytes :refer [b64->bytes bytes->b64]]
             [clojure.java.io :as io]
             [clojure.zip :as zip])
-  (:import [javax.crypto Cipher KeyGenerator]
-           [javax.crypto.spec GCMParameterSpec SecretKeySpec]
+  (:import [javax.crypto Cipher KeyGenerator SecretKeyFactory]
+           [javax.crypto.spec GCMParameterSpec IvParameterSpec SecretKeySpec PBEKeySpec]
            [java.security SecureRandom]
            [java.util.zip ZipEntry ZipOutputStream]))
-
-(defn encryption-key
-  [crypto-key]
-  (let [instance (KeyGenerator/getInstance "AES")
-        sr (SecureRandom/getInstance "SHA1PRNG")]
-    (.setSeed sr (.getBytes crypto-key "UTF-8"))
-    (.init instance 256 sr)
-    (.. instance generateKey getEncoded)))
-
-(defn generate-cipher
-  [mode crypto-key]
-  (let [key-spec (SecretKeySpec. (encryption-key crypto-key) "AES")
-        cipher (Cipher/getInstance "AES")]
-    (.init cipher mode key-spec)
-    cipher))
 
 (defn zip-dir
   [path]
@@ -31,16 +16,40 @@
         (io/copy f zip)
         (.closeEntry zip)))))
 
+(defn derive-key
+  [passphrase salt]
+  (let [key-spec (PBEKeySpec. (.toCharArray passphrase) salt 65536 256)]
+    (-> (SecretKeyFactory/getInstance "PBKDF2WithHmacSHA256")
+        (.generateSecret key-spec)
+        (.getEncoded))))
+
+(defn generate-cipher
+  [mode crypto-key salt iv]
+  (let [key-spec (SecretKeySpec. (derive-key crypto-key salt) "AES")
+        gcm-spec (GCMParameterSpec. 128 iv)
+        cipher (Cipher/getInstance "AES/GCM/NoPadding")]
+    (.init cipher mode key-spec gcm-spec)
+    cipher))
+
 (defn encrypt-file
-  [path crypto-key]
-  (let [plain-text (.getBytes (slurp path) "UTF-8")
-        cipher (generate-cipher Cipher/ENCRYPT_MODE crypto-key)
-        encrypted-text (bytes->b64 (.doFinal cipher plain-text))]
-    (spit path (String. encrypted-text))))
+  [path passphrase]
+  (let [plain-text (slurp path)
+        salt (byte-array 16)
+        iv (byte-array 12)
+        random (SecureRandom/getInstanceStrong)
+        key (derive-key passphrase salt)
+        gcm-spec (GCMParameterSpec. 128 iv)
+        cipher (Cipher/getInstance "AES/GCM/NoPadding")]
+    (.init cipher Cipher/ENCRYPT_MODE (SecretKeySpec. key "AES") gcm-spec)
+    (let [encrypted-text (.doFinal cipher (.getBytes plain-text "UTF-8"))
+          encrypted-data (concat salt iv encrypted-text)]
+      (with-open [out (io/output-stream path)]
+        (.write out (byte-array encrypted-data)))
+      path)))
 
 (defn encrypt-dir
   [dir-path crypto-key]
-  (let [zipped-dir (zip-dir dirpath)]
+  (let [zipped-dir (zip-dir dir-path)]
     (encrypt-file zipped-dir crypto-key)))
 
 (defn decrypt-dir
@@ -49,12 +58,24 @@
           :when (.exists (io/file file-contents))]
     (encrypt-file (.getPath file-contents) crypto-key)))
 
-(defn decrypt-file
-  [path crypto-key]
-  (let [file-contents (slurp path)
-        cipher (generate-cipher Cipher/DECRYPT_MODE crypto-key)
-        decrypted-text (try (.doFinal cipher (b64->bytes file-contents)) (catch Exception e nil))]
-    (if (nil? decrypted-text)
-      (println "Error decrypting file:" path)
-      (-> path
-          (spit (String. decrypted-text))))))
+;(defn decrypt-file
+;  [path crypto-key]
+;  (let [file-contents (slurp path)
+;        salt (subvec file-contents 0 16)
+;        iv (subvec file-contents 16 28)
+;        encrypted-text (subvec file-contents 28)
+;        cipher (generate-cipher Cipher/DECRYPT_MODE crypto-key salt iv)
+;        decrypted-text (.doFinal cipher encrypted-text)]
+;    (String. decrypted-text)))
+;(defn decrypt-file
+;  [path crypto-key]
+ ; (let [file-contents (slurp path)
+  ;      cipher (generate-cipher Cipher/DECRYPT_MODE crypto-key)
+   ;     decrypted-text ;(try 
+    ;    (.doFinal cipher (.getBytes (String. file-contents) "UTF-8")) ;(catch Exception e nil))
+;]
+ ;   (String. decrypted-text)))
+    ;(if (nil? decrypted-text)
+     ; (println "Error decrypting file:" path)
+      ;(-> path
+       ;   (spit (String. decrypted-text))))))
